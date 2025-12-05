b98c244ffcf205e0bfc30d9b027766608bd56301
Pavan Bellam <pavan94411@gmail.com>
2025-12-05 18:56:10 -0500

    Add SQS terraform module
    Creates main queue with dead letter queue:
    - Configurable visibility timeout (default 10 min)
    - Long polling enabled (20 sec)
    - 14 day message retention
    - Redrive policy to DLQ after 3 failures

---
+++ terraform/modules/sqs/main.tf
@@ -0,0 +1,18 @@
+# Dead Letter Queue
+resource "aws_sqs_queue" "dlq" {
+  name                      = "${var.queue_name}-dlq"
+  message_retention_seconds = var.dlq_message_retention_seconds
+}
+
+# Main Queue
+resource "aws_sqs_queue" "main" {
+  name                       = var.queue_name
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  message_retention_seconds  = var.message_retention_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq.arn
+    maxReceiveCount     = var.max_receive_count
+  })
+}
