52b5e0c1c859845470558c4a223c6bd7d2bafb40
Pavan Bellam <pavan94411@gmail.com>
2025-12-09 20:24:11 -0500

    Fix ECR module to output both producer and consumer repos

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,5 +1,5 @@
-resource "aws_ecr_repository" "this" {
-  name                 = var.repository_name
+resource "aws_ecr_repository" "producer" {
+  name                 = "${var.repository_name}-producer"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -7,9 +7,19 @@ resource "aws_ecr_repository" "this" {
   }
 }
 
+resource "aws_ecr_repository" "consumer" {
+  name                 = "${var.repository_name}-consumer"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+# Policy for CI/CD to push images - attach to OIDC role later
 resource "aws_iam_policy" "ecr_push" {
   name        = "${var.repository_name}-ecr-push-policy"
-  description = "Policy for pushing Docker images to ${var.repository_name}"
+  description = "Policy for pushing Docker images to ${var.repository_name} repositories"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -32,7 +42,10 @@ resource "aws_iam_policy" "ecr_push" {
           "ecr:UploadLayerPart",
           "ecr:CompleteLayerUpload"
         ]
-        Resource = aws_ecr_repository.this.arn
+        Resource = [
+          aws_ecr_repository.producer.arn,
+          aws_ecr_repository.consumer.arn
+        ]
       }
     ]
   })
--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,19 +1,24 @@
-output "repository_url" {
-  description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.this.repository_url
+output "producer_repository_url" {
+  description = "The URL of the producer ECR repository"
+  value       = aws_ecr_repository.producer.repository_url
 }
 
-output "repository_arn" {
-  description = "The ARN of the ECR repository"
-  value       = aws_ecr_repository.this.arn
+output "producer_repository_arn" {
+  description = "The ARN of the producer ECR repository"
+  value       = aws_ecr_repository.producer.arn
 }
 
-output "repository_name" {
-  description = "The name of the ECR repository"
-  value       = aws_ecr_repository.this.name
+output "consumer_repository_url" {
+  description = "The URL of the consumer ECR repository"
+  value       = aws_ecr_repository.consumer.repository_url
+}
+
+output "consumer_repository_arn" {
+  description = "The ARN of the consumer ECR repository"
+  value       = aws_ecr_repository.consumer.arn
 }
 
 output "push_policy_arn" {
-  description = "The ARN of the IAM policy for pushing images"
+  description = "The ARN of the IAM policy for pushing images (attach to OIDC role)"
   value       = aws_iam_policy.ecr_push.arn
 }
